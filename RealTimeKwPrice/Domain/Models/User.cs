f081f762448633e0ed59ee078061333a7600dcab
Nordstroem1 <johannord27@hotmail.com>
2024-12-22 20:39:02 +0100

    Created Userclass, kilowattprice-class and Roles

--- RealTimeKwPrice/Domain/Models/User.cs
+++ RealTimeKwPrice/Domain/Models/User.cs
@@ -1,6 +1,31 @@
-﻿namespace Domain.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models
 {
-    public class User
+    public class User : IdentityUser<Guid>
     {
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        [Required]
+
+        public RoleEnums.Roles Role { get; set; }
+        [Required]
+
+        public string Location { get; set; }
+        public List<KiloWattPrice> Price { get; set; }
+        public User(Guid userId, string userName, string email, string phoneNumber, string password, RoleEnums.Roles role, string location)
+        {
+            Id = userId;
+            UserName = userName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            PasswordHash = password;
+            Location = location;
+            CreatedAt = DateTime.Now;
+            LockoutEnabled = true;
+            LockoutEnd = null;
+            Role = role;
+        }
     }
 }
